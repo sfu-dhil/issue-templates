5d8a0a409dcb4174db17913bf774de3ebc48a5f4
Joey Takeda <joey.takeda@gmail.com>
2021-03-05 15:05:48 -0800

    More testing

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ async function go(){
         const repoTokens = repository.split("/");
         const owner = repoTokens[0];
         const repo = repoTokens[1];
+        const cfg = {
+            owner: owner,
+            repo: repo,
+            issue_number: issueNumber,
+            headers: {
+                "Accept": "application/vnd.github.v3.full+json"
+            }
+        };
         core.debug(`repository: ${repository}`);
         const octokit = github.getOctokit(token);
+        let thisIssue = await octokit.issues.get(cfg);
+        let body = thisIssue.body;
+        console.log(body);
+        console.log(thisIssue.body_html);
         await octokit.issues.addLabels({
             owner: owner,
             repo: repo,
