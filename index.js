62d95706096c7ebbb69bc23288b11c48e650e1ba
Joey Takeda <joey.takeda@gmail.com>
2021-03-05 14:39:20 -0800

    Add octokit

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const { inspect } = require("util");
 const core = require('@actions/core');
 const github = require('@actions/github');
+async function go(){
+    try{
+        const token = core.getInput('token');
+        const issueNumber = core.getInput('issue-number');
+        const repository = process.env.GITHUB_REPOSITORY;
+        const repoTokens = repository.split("/");
+        const owner = repoToken[0];
+        const repo = repoTokens[1];
+        core.debug(`repository: ${repository}`);
+        const octokit = github.getOctokit(token);
+        await octokit.issues.addLabels({
+            owner: owner,
+            repo: repo,
+            issue_number: issueNumber,
+            labels: ['test']
+        });
+        core.info(issueNumber);
 
-try{
-    const token = core.getInput('token');
-    const issueNumber = core.getInput('issue-number');
-    const repository = process.env.GITHUB_REPOSITORY;
-    const repoTokens = repository.split("/");
-    const owner = repoToken[0];
-    const repo = repoTokens[1];
-    core.debug(`repository: ${repository}`);
-    const octokit = github.getOctokit(token);
-    await octokit.issues.addLabels({
-        owner: owner,
-        repo: repo,
-        issue_number: issueNumber,
-        labels: ['test']
-    });
-    core.info(issueNumber);
-
-} catch (e){
-    console.log(e);
+    } catch (e){
+        console.log(e);
+    }
 }
+
+go().then(() => {
+    console.log('Finished!');
+})
